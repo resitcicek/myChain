150ef01e2bc8e113dffc276f5c0eab564d654e6c
boraktn <borakotan@posta.mu.edu.tr>
2022-05-19 17:20:52 +0300

    commit

---
+++ app/src/androidTest/java/mobile/resitcicek/mychain/DatabaseHelperTest.java
@@ -0,0 +1,70 @@
+package mobile.resitcicek.mychain;
+
+import junit.framework.TestCase;
+
+public class DatabaseHelperTest extends TestCase {
+
+    public void testOnCreate() {
+
+    }
+
+    public void testOnUpgrade() {
+    }
+
+    public void testReDB() {
+    }
+
+    public void testDone() {
+    }
+
+    public void testIsInChain() {
+    }
+
+    public void testUncheck() {
+    }
+
+    public void testIsDone() {
+    }
+
+    public void testInsertID() {
+    }
+
+    public void testGetDate() {
+    }
+
+    public void testGetUserCount() {
+    }
+
+    public void testInsert() {
+    }
+
+    public void testUpdate() {
+    }
+
+    public void testSearch() {
+    }
+
+    public void testInsertChain() {
+    }
+
+    public void testInsertRelation() {
+    }
+
+    public void testGetChains() {
+    }
+
+    public void testGetChainNumber() {
+    }
+
+    public void testGetRandom() {
+    }
+
+    public void testCheckUsername() {
+    }
+
+    public void testGetUser() {
+    }
+
+    public void testCheckLogin() {
+    }
+}
